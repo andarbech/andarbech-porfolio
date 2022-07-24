31c0f8e5278c5bfeabc3c1f2a5a5c997555fa899
andarbech <andarbech1@gmail.com>
2022-07-24 14:50:50 +0200

    base

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "./About/About";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <h1>Testing</h1>
+      <About />
+    </div>
+  );
+};
+export default App;
