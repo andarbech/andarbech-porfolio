ff3f058579eb22aede9b263e1e74f34b1d2581a3
andarbech <andarbech1@gmail.com>
2022-08-22 00:20:33 +0200

    need finishstyle

--- src/components/pages/Experience/Experience.js
+++ src/components/pages/Experience/Experience.js
@@ -3,50 +3,50 @@ import {BsPatchCheckFill} from "react-icons/bs";
 import "./experience.css";
 const Experience = () => {
   return (
-    <section id="experience">
+    <section className="container" id="experience">
       <h5>What Skills I have</h5>
       <h2>My Experience</h2>
-      <div className="container experience__container">
+      <div className="experience__container">
         <div className="experience__frontend">
           <h3>Frontend Developer</h3>
           <div className="experience__content">
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>HTML</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>CSS</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>JavaScript</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Bootstrap</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>React</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Typescript</h4>
                 <small className="text-light">testing</small>
@@ -57,45 +57,78 @@ const Experience = () => {
         <div className="experience__backend">
           <h3>Backend Developer</h3>
           <div className="experience__content">
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>NodeJs</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Mongo</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>PHP</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>MySql</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Python</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
-            <article className="experince_details">
-              <BsPatchCheckFill />
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>Symfony</h4>
+                <small className="text-light">testing</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__microsoft">
+          <h3>Microsoft Developer </h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Azure</h4>
+                <small className="text-light">testing</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PowerAutomate</h4>
+                <small className="text-light">testing</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PowerApps</h4>
+                <small className="text-light">testing</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>PowerBI</h4>
                 <small className="text-light">testing</small>
               </div>
             </article>
