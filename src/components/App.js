21d8522f0fa9e110ce8a917bcd969361560a0754
andarbech <andarbech1@gmail.com>
2022-08-23 01:38:41 +0200

    need finish emailsend

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import About from "./pages/About/About";
 import HeaderComponent from "./organism/Header";
 import NavigationComponent from "./pages/Navigation";
 import Experience from "./pages/Experience";
-import Services from "./pages/Services";
-import Porfolio from "./pages/Porfolio";
-import Testimonials from "./pages/Testimonials";
+// import Services from "./pages/Services";
+// import Porfolio from "./pages/Porfolio";
+// import Testimonials from "./pages/Testimonials";
 import Footer from "./organism/Footer";
 import Contact from "./pages/Contact";
 
